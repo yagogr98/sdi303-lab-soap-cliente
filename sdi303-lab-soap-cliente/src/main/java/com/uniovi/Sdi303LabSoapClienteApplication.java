64deaade392b58a9111f21a37ec95c7f288c60be
yagogr98 <yagogr98@gmail.com>
2019-04-25 20:41:40 +0200

    SDI-SW-11.2 Servicio SOAP Cliente

---
+++ sdi303-lab-soap-cliente/src/main/java/com/uniovi/Sdi303LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi303LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi303LabSoapClienteApplication.class, args);
+	}
+
+}
